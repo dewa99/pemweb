b7d8b8b0edb6b6d318ffdd5cd2a4191d613a3284
JohnKontolatos <33590458+dewa99@users.noreply.github.com>
2018-11-09 14:43:36 +0700

    Add files via upload

---
+++ Upload/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tes</title>
+</head>
+<body>
+
+<form name="form" method="post" enctype="multipart/form-data" action="upload.php">
+Image : <input name="picture" type="file" />
+<input type="submit" name="upload" value="Upload" />
+
+</form>
+</body>
+</html>
